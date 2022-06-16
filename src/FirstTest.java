92ee62b4bb97a2759efa894109b0fc3324e28125
Mikhail Zorin <jiopgzorin@icloud.com>
2022-06-16 14:50:07 +0300

    Урок 3, Ex2: Создание метода - выполнена

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,15 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import sun.plugin.dom.core.Element;
 
 import java.net.URL;
 
@@ -24,6 +30,7 @@ public class FirstTest {
         capabilities.setCapability("appActivity", ".main.MainActivity");
         capabilities.setCapability("app", "C:/Users/mike.zorin/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
+
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
@@ -34,8 +41,27 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest() {
 
-        System.out.println("First test run");
+    public void assertElementHasTextTest() {
+
+
+        WebElement element = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
+        element.click();
+
+        WebElement waitForElementByXpath = assertElementHasText(
+                "//*[contains(@text,'Search…')]",
+                "The element for this locator does not contain text!",
+                5);
+
+        Assert.assertEquals("The element does not match the expected value!", "Search…", waitForElementByXpath.getText());
+    }
+
+
+    private WebElement assertElementHasText(String xpath, String errorMessage, long timeoutInSeconds) {
+
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 }
