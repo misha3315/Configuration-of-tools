f575effec024cd003543131c8a5956aeeac17c0f
Mikhail Zorin <jiopgzorin@icloud.com>
2022-07-15 11:51:23 +0300

    Урок 4, Ex6: Тест: assert title- Выполнено

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,17 +1,16 @@
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.List;
 
 public class FirstTest {
 
@@ -40,111 +39,32 @@ public class FirstTest {
         driver.quit();
     }
 
+
     @Test
-    public void assertElementHasTextTest() {
-
+    public void testAmountOfEmptySearch() {
 
         waitForElementByXpathAndClick(
                 By.xpath("//*[@text='Search Wikipedia']"),
                 "The element did not open!",
                 5);
+        String searchLine = "Java";
         waitForElementByIdAndSandKeys(
                 By.xpath("//*[@text='Search…']"),
-                "Java",
-                "The element for this locator does not contain text!",
+                searchLine,
+                "Cannot find search input",
                 5);
-        waitForElementByXpathAndClick(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
-                "Cannot find 'Search Wikipedia' input",
-                5);
+
+        String searchResultLocator = "//android.widget.TextView[@test='Java']";
+        String emptyResultLabel = "//*[@text='Java']";
+
         assertElementHasText(
-                By.id("org.wikipedia:id/view_page_title_text"),
-                "Cannot find article title",
+                By.xpath(emptyResultLabel),
+                "Cannot find empty result label by the request " + searchLine + " ",
                 15);
-        waitForElementByIdAndClick(
-                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
-                "Cannot find button to open article options",
-                5);
-        waitForElementByIdAndClick(
-                By.xpath("//*[@text='Add to reading list']"),
-                "Cannot find option to add article to reading list",
-                5);
-        waitForElementByIdAndClick(
-                By.id("org.wikipedia:id/onboarding_button"),
-                "Cannot find 'Got it' tip overlay",
-                5);
-        waitForElementAndClear(
-                By.id("org.wikipedia:id/text_input"),
-                "Cannot delete 'My reading list'",
-                5);
-        String nameOfFolder = "Learning programming";
-        waitForElementByIdAndSandKeys(
-                By.id("org.wikipedia:id/text_input"),
-                nameOfFolder,
-                "Cannot pur text 'Learning programming' in article title",
-                5);
-        waitForElementByIdAndClick(
-                By.xpath("//*[@text='OK']"),
-                "Cannot press 'Ok' button ",
-                5);
-        waitForElementByIdAndClick(
-                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
-                "Cannot close article, cannot find X link",
-                5);
-        waitForElementByXpathAndClick(
-                By.xpath("//*[@text='Search Wikipedia']"),
-                "The element did not open!",
-                5);
-        waitForElementByIdAndSandKeys(
-                By.xpath("//*[@text='Search…']"),
-                "Java",
-                "The element for this locator does not contain text!",
-                5);
-        waitForElementByXpathAndClick(
-                By.xpath("//android.widget.TextView[@text='Island of Indonesia, Southeast Asia']"),
-                "Cannot click to element Java",
-                5);
-        waitForElementByIdAndClick(
-                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
-                "Cannot find button to open article options",
-                5);
-        waitForElementByIdAndClick(
-                By.xpath("//*[@text='Add to reading list']"),
-                "Cannot find option to add article to reading list",
-                5);
-        waitForElementByXpathAndClick(
-                By.xpath("//android.widget.TextView[@text='Learning programming']"),
-                "Cannot click to "+ nameOfFolder+ "",
-                5);
-        waitForElementByIdAndClick(
-                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
-                "Cannot close article, cannot find X link",
-                5);
-        waitForElementByXpathAndClick(
-                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
-                "Cannot find navigate button My list",
-                5);
-                waitForElementByIdAndClick(
-                By.xpath("//*[@text='" + nameOfFolder + "']"),
-                "Cannot find created folder",
-                5);
-              swipeElementToLeft(
-              By.xpath("//*[@text='Java (programming language)']"),
-              "Cannot find saved folder");
-        waitForElementNotPresent(
-                By.xpath("//*[@text='Island of Indonesia, Southeast Asia']"),
-                "Cannot delete saved article",
-                5);
-        waitForElementByXpathAndClick(
-                By.xpath("//android.widget.TextView[@text='Java']"),
-                "Cannot click to element Java",
-                5);
-        assertElementHasText(
-                By.xpath("//android.widget.TextView[@text='Java']"),
-                "The article is missing");
-
-         }
-
+        assertElementPresent(
+                By.xpath(searchResultLocator),
+                "We've found same result by request " + searchLine);
+    }
 
     private WebElement assertElementHasText(By by, String errorMessage, long timeoutInSeconds) {
 
@@ -165,92 +85,26 @@ public class FirstTest {
         return element;
     }
 
-    private WebElement waitForElementByXpathAndSandKeys(By by, String value, String errorMessage, long timeoutInSeconds) {
-        WebElement element = assertElementHasText(by, errorMessage, 5);
-        element.sendKeys(value);
-        return element;
-    }
-
-    private WebElement waitForElementByIdAndClick(By by, String errorMessage, long timeoutInSeconds) {
-        WebElement element = assertElementHasText(by, errorMessage, 5);
-        element.click();
-        return element;
-    }
 
     private WebElement waitForElementByIdAndSandKeys(By by, String value, String errorMessage, long timeoutInSeconds) {
         WebElement element = assertElementHasText(by, errorMessage, 5);
         element.sendKeys(value);
         return element;
     }
-    protected boolean waitForElementNotPresent(By by, String errorMessage, long timeoutInSeconds){
 
-        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
-        wait.withMessage(errorMessage + "\n");
-        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+
+    private int getAmountOfElements(By by) {
+        List elements = driver.findElements(by);
+        return elements.size();
     }
 
-    private WebElement waitForElementAndClear(By by, String errorMessage, long timeoutInSeconds) {
-        WebElement element = assertElementHasText(by, errorMessage, timeoutInSeconds);
-        element.clear();
-        return element;
+    private void assertElementPresent(By by, String errorMessage) {
+        int amountOfElements = getAmountOfElements(by);
 
-
-
-    }
-
-    protected void swipeUp(int timeOfSwipe) {
-
-        TouchAction action = new TouchAction(driver);
-
-        Dimension size = driver.manage().window().getSize();
-        int x = size.width / 2;
-        int start_y = (int) (size.height * 0.9);
-        int end_y = (int) (size.height * 0.1);
-
-        action
-                .press(x, start_y)
-                .waitAction(timeOfSwipe)
-                .moveTo(x, end_y)
-                .release()
-                .perform();
-    }
-
-    protected void swipeQuick() {
-        swipeUp(400);
-    }
-
-    protected void swipeUpFindElemet(By by, String errorMessage, int maxSwiped) {
-
-        int already = 0;
-        while (driver.findElements(by).size() == 0) {
-            if (already > maxSwiped) {
-                assertElementHasText(by, "Cannot find element \n" + errorMessage, 0);
-                return;
-            }
-            swipeQuick();
-            ++already;
-
+        if (amountOfElements < 0) {
+            String defaultMessage = "An element '" + by.toString() + "' supposed to be noy present";
+            throw new AssertionError(defaultMessage + " " + errorMessage);
         }
-    }
 
-    protected void swipeElementToLeft(By by, String errorMessage) {
-        WebElement element = assertElementHasText(
-                by,
-                errorMessage,
-                15);
-
-        int left_x = element.getLocation().getX();
-        int right_x = left_x + element.getSize().getWidth();
-        int upper_y = element.getLocation().getY();
-        int lower_y = upper_y + element.getSize().getHeight();
-        int middle_y = (upper_y + lower_y) / 2;
-
-        TouchAction action = new TouchAction(driver);
-        action
-                .press(right_x, middle_y)
-                .waitAction(400)
-                .moveTo(left_x, middle_y)
-                .release()
-                .perform();
     }
 }
